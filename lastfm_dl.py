fa339f9344cf3271397069f03206c5373cbdbd55
tofu-connected <tofu.connected@gmail.com>
2024-05-30 22:22:49 +0900

    Update lastfm_dl.py

--- lastfm_dl.py
+++ lastfm_dl.py
@@ -3,6 +3,8 @@
 # Dependencies: pylast requests pytube mutagen pillow
 # ffmpeg must be installed in the system!
 #
+# DISCLAIMER: It takes the first result from YouTube search so it often gets it wrong. Check your audio!
+#
 import os
 import pylast
 import requests
@@ -15,11 +17,14 @@ from PIL import Image
 import subprocess
 import difflib
 import shutil
+from datetime import datetime
 
-lastfm_api_key = "YOUR_LASTFM_API_KEY"
-lastfm_api_secret = "YOUR_LASTFM_API_SHARED_SECRET"
-lastfm_username = "LASTFM_USERNAME"
-
+LASTFM_API_KEY = "YOUR_LASTFM_API_KEY"
+LASTFM_API_SECRET = "YOUR_LASTFM_API_SHARED_SECRET"
+LASTFM_USERNAME = "LASTFM_USERNAME"
+DL_DIR = "lastfm_dl"
+MAX_LENGTH = 600
+FROM_DATE = "01-01-1900"
 
 def search_youtube_link(search_query):
     url = f"https://www.youtube.com/results?search_query={search_query}"
@@ -39,14 +44,22 @@ def write_mp3_tags(filename, artist_name, track_name, thumbnail_data):
 
     thumbnail = Image.open(thumbnail_data)
     thumbnail = thumbnail.resize(
-        (500, 500), Image.LANCZOS)  # Resize to 500x500
-    thumbnail = thumbnail.crop((0, 0, 500, 500))  # Crop to square
+        (500, 500), Image.LANCZOS)
+    thumbnail = thumbnail.crop((0, 0, 500, 500))
     thumbnail_bytes = BytesIO()
     thumbnail.save(thumbnail_bytes, format='PNG')
     thumbnail_bytes = thumbnail_bytes.getvalue()
-    audio.tags.add(APIC(3, 'image/png', 3, 'Front cover', thumbnail_bytes))
+    audio.tags.add(APIC(3, 'image/png', 3, 'cover', thumbnail_bytes))
 
     audio.save()
+
+def is_processed(artist_name: str, track_name: str):
+    for file in os.listdir(DL_DIR):
+        if file.endswith(".mp3"):
+            mp3 = MP3(f"{DL_DIR}\\{file}", ID3=ID3)
+            if "TPE1" in mp3.tags and mp3.tags["TPE1"].text[0] == artist_name and "TIT2" in mp3.tags and mp3.tags["TIT2"][0] == track_name:
+               return True 
+    return False
 
 
 def main():
@@ -54,45 +67,56 @@ def main():
         raise Exception("ffmpeg not found!")
 
     network = pylast.LastFMNetwork(
-        api_key=lastfm_api_key,
-        api_secret=lastfm_api_secret
+        api_key=LASTFM_API_KEY,
+        api_secret=LASTFM_API_SECRET
     )
-    user = network.get_user(lastfm_username)
+    user = network.get_user(LASTFM_USERNAME)
 
     loved_tracks = user.get_loved_tracks(limit=None)
+
+    from_date = datetime.strptime(FROM_DATE, '%d-%m-%Y')
+    loved_tracks = [track for track in loved_tracks if datetime.strptime(track.date, '%d %b %Y, %H:%M') >= from_date]
 
     for i, track in enumerate(loved_tracks):
         artist_name = track.track.artist.name
         track_name = track.track.title
 
-        filename = f"{artist_name} - {track_name}"
-        filename = "".join(
-            [x if x not in "\\/:?\"<>|*" else "_" for x in filename])
-        filename = f"lastfm_dl\\{filename}"
-        filename_mp3 = f"{filename}.mp3"
-
         progress = f"{i+1}/{len(loved_tracks)}"
 
-        if os.path.isfile(filename_mp3):
-            print(f"⏩ {progress} Skipped: {filename_mp3}")
+        if is_processed(artist_name, track_name):
+            print(f"⏩ {progress} Skipped: {artist_name} - {track_name}")
             continue
 
         search_query = f"{track_name} {artist_name}"
+
         youtube_link = search_youtube_link(search_query)
 
         if youtube_link:
             yt = YouTube(youtube_link)
-            audio = sorted(yt.streams.filter(type="audio"), key=lambda s: int(
-                str(s.abr).rstrip("kbps")), reverse=True)[0]
 
             video_title = yt.title
 
-            if not os.path.exists("lastfm_dl"):
-                os.makedirs("lastfm_dl")
+            if yt.length > MAX_LENGTH:
+                print(f"❌ {progress} Maximum length exceeded. Searched '{track_name} {artist_name}', found '{video_title}'")
+                continue
+
+            audio = sorted(yt.streams.filter(type="audio"), key=lambda s: int(
+                str(s.abr).rstrip("kbps")), reverse=True
+            )[0]
+
+            filename = video_title
+            filename = "".join(
+                [x if x not in "\\/:?\"<>|*" else "_" for x in filename]
+            )
+            filename = f"{DL_DIR}\\{filename}"
+            filename_mp3 = f"{filename}.mp3"
+
+            if not os.path.exists(DL_DIR):
+                os.makedirs(DL_DIR)
 
             audio.download(output_path=".", filename=filename)
 
-            if audio.audio_codec == "opus":
+            if audio.audio_codec in ["opus", "mp4a.40.2"]:
                 command = ["ffmpeg", "-i", filename, "-vn", "-ar", "44100", "-ac", "2", "-b:a",
                            "160k", "-f", "mp3", filename_mp3, "-y", "-hide_banner", "-loglevel", "error"]
                 subprocess.call(command, shell=True)
@@ -105,7 +129,7 @@ def main():
                 print(
                     f"{'✅' if diff_ratio > 0.3 else '🤔'} {progress} Searched '{track_name} {artist_name}', found '{video_title}' (similarity {round(diff_ratio, 2)})   Converted [{audio.audio_codec}, {audio.abr}] to [mp3, 160kbps]")
             else:
-                print(f"❓{progress} Unknown audio codec: {audio.audio_codec}")
+                print(f"❓ {progress} Unknown audio codec: {audio.audio_codec}")
                 continue
 
             thumbnail_data = requests.get(yt.thumbnail_url, stream=True)
@@ -114,8 +138,7 @@ def main():
             write_mp3_tags(filename_mp3, artist_name,
                            track_name, thumbnail_data.raw)
         else:
-            print(
-                f"❌ {progress} No YouTube video found for '{artist_name} - {track_name}'")
+            print(f"❌ {progress} No YouTube video found for '{artist_name} - {track_name}'")
 
 
 if __name__ == "__main__":
